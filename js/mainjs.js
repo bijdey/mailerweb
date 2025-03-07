2ab822cfb257d9a8b8845a345bff25529b6c512a
bijdey <creativenetwork.bijay@gmail.com>
2025-03-07 11:18:01 +0530

    first code

---
+++ js/mainjs.js
@@ -0,0 +1,28 @@
+﻿function openPopup() {
+
+    if (document.getElementById("popup").style.display === "block")
+        document.getElementById("popup").style.display = "none";
+    else
+        document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    let navLinks = document.querySelectorAll(".menu-item");
+    let navbarToggler = document.querySelector(".navbar-toggler");
+    let navbarCollapse = document.querySelector("#navbarNav");
+
+    navLinks.forEach(function (link) {
+        link.addEventListener("click", function () {
+            if (window.innerWidth < 992) { // Close menu only in responsive mode
+                let bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
+                if (bsCollapse) {
+                    bsCollapse.hide();
+                }
+            }
+        });
+    });
+});
